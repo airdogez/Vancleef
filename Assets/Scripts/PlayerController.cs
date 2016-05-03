b6cf60858a08a50bb95a9fa1e4e2141000f26167
Andres Revolledo <aqwssama@gmail.com>
2016-05-03 15:34:39 -0500

    Added basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+    float x = Input.GetAxis("Horizontal");
+    float y = Input.GetAxis("Vertical");
+
+    Vector3 pos = gameObject.transform.localPosition;
+    pos.x += x/10f;
+    pos.y += y/12f;
+    gameObject.transform.localPosition = pos;
+
+  }
+}
--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class Util : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
