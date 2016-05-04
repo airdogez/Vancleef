b138a5082e2b95153a80ccb201568a12901ae1f9
Andres Revolledo <aqwssama@gmail.com>
2016-05-03 21:53:19 -0500

    Added Bullets

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserController : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+    Vector3 pos = gameObject.transform.localPosition;
+    pos.y += 0.5f;
+    gameObject.transform.localPosition = pos;
+
+  }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 public class PlayerController : MonoBehaviour {
 
+  private const int NUMBER_BULLETS = 10;
+
+  private List<GameObject> bullets;
+  private int currentBullet;
   // Use this for initialization
   void Start () {
     //Set initial position of player 
@@ -15,11 +21,23 @@ public class PlayerController : MonoBehaviour {
     pos.y = Mathf.Lerp(rightTop.y, leftBottom.y, 0.85f);
 
     gameObject.transform.localPosition = pos;
+    
+
+    //Create reusable bullets
+    bullets = new List<GameObject>();
+    for (int i = 0; i < NUMBER_BULLETS; i++)
+    {
+      GameObject bullet = Util.LoadPFab("Prefabs/prefab_basic_bullet");
+      bullet.active = false;
+      bullets.Add(bullet);
+    }
+    currentBullet = 0;
 
   }
 
   // Update is called once per frame
   void Update () {
+    //Movement
     float x = Input.GetAxis("Horizontal");
     float y = Input.GetAxis("Vertical");
     bool shift = Input.GetButton("Slow Down");
@@ -28,5 +46,18 @@ public class PlayerController : MonoBehaviour {
     pos.x += x/mod;
     pos.y += y/mod;
     gameObject.transform.localPosition = pos;
+
+    //Shoot Bullets
+    //Set initial position and activate
+    if (currentBullet >= NUMBER_BULLETS)
+      currentBullet = 0;
+    if (Input.GetButtonDown("Primary Fire")) {
+      GameObject bullet = bullets[currentBullet++];
+      Vector3 bullet_position = bullet.transform.localPosition;
+      bullet_position.x = transform.localPosition.x;
+      bullet_position.y = transform.localPosition.y;
+      bullet.transform.localPosition = pos;
+      bullet.active = true;
+    }
   }
 }
