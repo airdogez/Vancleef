d7ef54392ba17baf0576c41ec886db493491f99d
Andres Revolledo <aqwssama@gmail.com>
2016-05-03 22:27:31 -0500

    Basic screen limits for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,24 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
 
-  private const int NUMBER_BULLETS = 10;
+  private const int NUMBER_BULLETS = 3;
 
   private List<GameObject> bullets;
   private int currentBullet;
+  private Vector3 leftBottom;
+  private Vector3 rightTop;
   // Use this for initialization
   void Start () {
     //Set initial position of player 
-    Vector3 leftBottom = Vector3.zero;
-    Vector3 rightTop = Vector3.zero;
+    leftBottom = Vector3.zero;
+    rightTop = Vector3.zero;
     Util.ComputeResponsiveScreenPoints(Camera.allCameras[0], out leftBottom, out rightTop);
 
     Vector3 pos = gameObject.transform.localPosition;
 
-    pos.x = Mathf.Lerp(leftBottom.x, rightTop.x, 0.5f);
-    pos.y = Mathf.Lerp(rightTop.y, leftBottom.y, 0.85f);
+      pos.x = Mathf.Lerp(leftBottom.x, rightTop.x, 0.5f);
+
+      pos.y = Mathf.Lerp(rightTop.y, leftBottom.y, 0.85f);
 
     gameObject.transform.localPosition = pos;
     
@@ -45,6 +48,17 @@ public class PlayerController : MonoBehaviour {
     Vector3 pos = gameObject.transform.localPosition;
     pos.x += x/mod;
     pos.y += y/mod;
+
+    if (pos.x > rightTop.x)
+      pos.x = rightTop.x;
+    if (pos.x < leftBottom.x)
+      pos.x =leftBottom.x;
+
+    if (pos.y > rightTop.y)
+      pos.y = rightTop.y;
+    if (pos.y < leftBottom.y)
+      pos.y =leftBottom.y;
+
     gameObject.transform.localPosition = pos;
 
     //Shoot Bullets
@@ -53,11 +67,20 @@ public class PlayerController : MonoBehaviour {
       currentBullet = 0;
     if (Input.GetButtonDown("Primary Fire")) {
       GameObject bullet = bullets[currentBullet++];
-      Vector3 bullet_position = bullet.transform.localPosition;
-      bullet_position.x = transform.localPosition.x;
-      bullet_position.y = transform.localPosition.y;
-      bullet.transform.localPosition = pos;
-      bullet.active = true;
+      if (!bullet.active){
+        Vector3 bullet_position = bullet.transform.localPosition;
+        bullet_position.x = transform.localPosition.x;
+        bullet_position.y = transform.localPosition.y;
+        bullet.transform.localPosition = pos;
+        bullet.active = true;
+      }
+    }
+    //If bullet is outside disabled it
+    foreach (var bullet in bullets)
+    {
+      if(bullet.transform.localPosition.y > rightTop.y){
+        bullet.active = false;
+      }
     }
   }
 }
