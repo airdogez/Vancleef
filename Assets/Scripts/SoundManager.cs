811571dd193a02415ac13eee0d625d40ece7d5de
Andres Revolledo <aqwssama@gmail.com>
2016-06-24 02:01:31 -0500

    REvert back, Game scene error

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour {
+
+  public Dictionary<string, AudioSource> mAudioDictionary;
+  // Use this for initialization
+  void Start () {
+    mAudioDictionary = new Dictionary<string, AudioSource>();
+    mAudioDictionary.Add("shoot", new AudioSource());
+    mAudioDictionary.Add("explosion", new AudioSource());
+    mAudioDictionary.Add("diamond", new AudioSource());
+    mAudioDictionary.Add("bgm", new AudioSource());
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+}
